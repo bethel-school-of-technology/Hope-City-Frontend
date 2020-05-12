dde9454e1f83c8f18af1491a7fda1e24d6d731c9
Sean Rathbun <seanfrathbun@gmail.com>
2020-05-11 22:38:55 -0400

    added routes and simple navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
+
+// authorization
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+
+// layouts
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+
+// views
+import { HomeComponent } from './views/home/home.component';
+import { EventsComponent } from './views/events/events.component';
+import { PrayerComponent } from './views/prayer/prayer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    EventsComponent,
+    PrayerComponent
   ],
   imports: [
     BrowserModule,
