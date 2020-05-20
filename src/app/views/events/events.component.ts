616d751cc1cca81eaefae7f775d595f783f5d927
Sean Rathbun <seanfrathbun@gmail.com>
2020-05-20 08:22:58 -0400

    Got mockdata posting to events page with *ngFor

--- src/app/views/events/events.component.ts
+++ src/app/views/events/events.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventsService } from '../events.service'
-
+import { Events } from '../events.model';
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
@@ -8,14 +8,48 @@ import { EventsService } from '../events.service'
 })
 export class EventsComponent implements OnInit {
 
-  public events = [];
+  events: Events[];
 
-  constructor(private EventsService: EventsService) { }
+  // events: Events[] = [
+      // {
+      //   eventName: "This is the event name",
+      //   eventInfo: "Event info is here",
+      //   eventAddress: "Testing Event Address",
+      //   eventCity: "Testing Event City",
+      //   eventState: "Testing Event State",
+      //   eventZip: 29715,
+      //   eventDay: new Date(2020, 2, 22)
+      // }
+    // ]
 
-  ngOnInit() {
-
-    this.EventsService.getEvents()
-    .subscribe(allEvents => this.events = allEvents);
+  constructor(private eventsService: EventsService) {
   }
 
-}
+    // getEvents() {
+    //   this.eventsService
+    //   .getEvents()
+    //   console.log("Get Events", this.eventsService)
+    // }
+
+    // getEvents() {
+    //   this.events = eventsService.events
+    //   console.log("Get Events", this.eventsService)
+    // }
+
+
+  ngOnInit() {
+   this.eventsService.getEvents()
+   .subscribe(events => this.events = events)
+    // this.getEvents()
+    // console.log("ngOnInit working", this.eventsService)
+  }
+
+  // ngOnInit() {
+  //   this.eventsService.getEvents()
+  //   console.log("ngOnInit working", this.events)
+  // }
+
+  // this.getEvents().subscribe(events => this.events = events);
+
+  }
+
